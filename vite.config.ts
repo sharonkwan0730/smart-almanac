a39ee9e19bfd2657e82db71092d7d54843ab0b45
sharonkwan0730 <sharonkwan0730@gmail.com>
2026-02-02 23:09:16 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,10 @@
-
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  // 載入當前環境的變數（包含 Vercel 注入的系統變數）
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    plugins: [react()],
-    define: {
-      'process.env': {
-        API_KEY: JSON.stringify(env.API_KEY || '')
-      }
-    },
-    build: {
-      outDir: 'dist',
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  build: {
+    outDir: 'dist',
+  }
 });
