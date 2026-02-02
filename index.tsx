0e7cac3b33d1123a21de9cc7c0952df007249e57
sharonkwan0730 <sharonkwan0730@gmail.com>
2026-02-03 00:16:35 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,14 +2,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // 💡 確保這行存在，且根目錄要有 index.css
+import './index.css'; // 💡 務必確保 GitHub 上有 index.css
 
 const rootElement = document.getElementById('root');
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
+  root.render(<React.StrictMode><App /></React.StrictMode>);
 }
